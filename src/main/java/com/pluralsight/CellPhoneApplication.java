540fe507bce8cb705b6918bec4d3a4e805667b5e
kadillon21 <kadillon21@gmail.com>
2026-04-16 14:47:26 -0500

    Added methods to clean up CellPhoneApplication

--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -4,11 +4,17 @@ import java.util.Scanner;
 
 public class CellPhoneApplication {
 
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         CellPhone usersPhone = new CellPhone();
+
+        addPhoneNumber(scanner, usersPhone);
+        phoneInformation(usersPhone);
+    }
+
+    public static void addPhoneNumber(Scanner scanner, CellPhone usersPhone){
 
         System.out.print("What is the serial number?: ");
         usersPhone.setSerialNumber(scanner.nextInt());
@@ -21,13 +27,22 @@ public class CellPhoneApplication {
         usersPhone.setPhoneNumber(scanner.nextLine());
         System.out.print("Who is the owner of the phone?: ");
         usersPhone.setOwner(scanner.nextLine());
+        System.out.println();
+
+    }
+
+    public static void phoneInformation(CellPhone userPhone){
+
+        String RED = "\u001B[31m";
+        String RESET = "\u001B[0m";
+
+        System.out.println("Serial Number: " + RED + userPhone.getSerialNumber() + RESET);
+        System.out.println("Model: " + RED + userPhone.getModel() + RESET);
+        System.out.println("Carrier: " + RED + userPhone.getCarrier() + RESET);
+        System.out.println("Phone Number: " + RED + userPhone.getPhoneNumber() + RESET);
+        System.out.println("Owner: " + RED + userPhone.getOwner() + RESET);
 
 
-        System.out.println(usersPhone.getModel());
-        System.out.println(usersPhone.getOwner());
-        System.out.println(usersPhone.getSerialNumber());
-        System.out.println(usersPhone.getCarrier());
-        System.out.println(usersPhone.getPhoneNumber());
     }
 
 }
