c419a27bafcff9445548a9820194e9c63e9b5757
kadillon21 <kadillon21@gmail.com>
2026-04-16 14:06:16 -0500

    Progress made

--- src/main/java/com/pluralsight/CellPhone.java
+++ src/main/java/com/pluralsight/CellPhone.java
@@ -1,4 +1,49 @@
 package com.pluralsight;
 
 public class CellPhone {
+
+    public int serialNumber;
+    public String model;
+    public String carrier;
+    public String phoneNumber;
+    public String owner;
+
+    public CellPhone(int serialNumber, String model, String carrier, String phoneNumber, String owner){
+
+        this.serialNumber = serialNumber;
+        this.model = model;
+        this.carrier = carrier;
+        this.phoneNumber = phoneNumber;
+        this.owner = owner;
+
+    }
+
+    public CellPhone() {
+        serialNumber = 0;
+        model = "";
+        carrier = "";
+        phoneNumber = "";
+        owner = "";
+    }
+
+
+    public void setSerialNumber(int serialNumber){
+        this.serialNumber = serialNumber;
+    }
+
+    public void setModel(String model){
+            this.model = model;
+    }
+
+    public void setCarrier(String carrier){
+        this.carrier = carrier;
+    }
+
+    public void setPhoneNumber(int phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setOwner(String owner){
+        this.owner = owner;
+    }
 }
--- src/main/java/com/pluralsight/CellPhoneApplication.java
+++ src/main/java/com/pluralsight/CellPhoneApplication.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellPhoneApplication {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        CellPhone usersPhone = new CellPhone();
+
+        /*
+         usersPhone.setSerialNumber(scanner.nextInt());
+         usersPhone.setModel(scanner.nextLine());
+         usersPhone.setCarrier(scanner.nextLine());
+         usersPhone.setPhoneNumber(scanner.nextInt());
+         usersPhone.setOwner(scanner.nextLine());
+        */
+
+        System.out.println("What is the serial number?: ");
+        usersPhone.setSerialNumber();
+        System.out.println("What model is the phone?: ");
+        System.out.println("Who is the carrier?: ");
+        System.out.println("What is the phone number?: ");
+        System.out.println("Who is the owner of the phone?: ");
+
+    }
+
 }
